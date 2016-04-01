11ea97a09a14f1f8ecc9e019d3176c3e78a0b20d
Adam Oswalt <adam.oswalt@gmail.com>
2016-03-31 22:38:07 -0500

    Added Game of Life rule logic.

--- life.js
+++ life.js
@@ -39,6 +39,43 @@ function updateCells() {
     });
 }
 
+function getNeighborCount(x, y) {
+    var count = 0;
+    for(var yOff = -1; yOff !== 2; ++yOff) {
+        if((y - yOff < 0) || (y + yOff >= height)) continue;
+        for(var xOff = -1; xOff !== 2; ++xOff) {
+            if((x - xOff < 0) || (x + xOff >= width)) continue;
+            if(xOff === 0 && yOff === 0) continue;
+
+            var id = get1dIndex(x + xOff, y + yOff);
+            if(activeCellList[id]) ++count;
+        }
+    }
+    return count;
+}
+
+function tick() {
+    for(var y = 0; y != height; ++y) {
+        for(var x = 0; x != width; ++x) {
+            // < 2 dies
+            // > 3 dies
+            // 2 3 stay
+            // = 3 life
+            var id = get1dIndex(x, y);
+            var neighbors = getNeighborCount(x, y);
+            if(neighbors < 2 || neighbors > 3) {
+                tempCellList[id] = false;
+            } else if(neighbors === 2) {
+                tempCellList[id] = activeCellList[id];
+            } else {
+                tempCellList[id] = true;
+            }
+        }
+    }
+
+    updateCells();
+}
+
 
 //NOTE(adam): fill the grid array with references to the cells for easier access
 $("tr").each(function(r, row) {
@@ -56,8 +93,13 @@ for(var i = 0; i != cellCount; ++i) {
 }
 
 
+setCellLife(2, 4, true);
+setCellLife(3, 4, true);
 setCellLife(4, 4, true);
+setCellLife(4, 3, true);
+setCellLife(3, 2, true);
 setCellLife(2, 7, true);
 updateCells();
+tick();
 
 //alert(cellCount + "  " + width + "  " + height + "  " + activeCellList.length);
